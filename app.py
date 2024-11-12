19f455ede9d8fb974a8220ce52971aab580f8ded
Kingslyno <kingslyno@gmail.com>
2024-11-12 21:12:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,11 @@ def submit():
     user = User(name, age, gender, total_income, expenses)
     collection.insert_one(user.__dict__)
     user.save_to_csv()
-
-    return redirect('/')
+     
+    return redirect('/successful')
+@app.route('/success')
+def success():
+    return "Data submitted successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
