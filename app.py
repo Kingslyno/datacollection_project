847fd3a04ab4a79e4e593aff462e57d5f2d62a77
Kingslyno <kingslyno@gmail.com>
2024-11-17 10:36:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import  pymongo
 import csv
 import pandas as pd
 
+#DB Connection
 connection_url = 'mongodb+srv://mydbuser:<Edison123>@cluster0.tdrd7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 app = Flask(__name__, template_folder='views')
 client = pymongo.MongoClient(connection_url)
