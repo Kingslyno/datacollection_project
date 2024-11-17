385df9d3f483af8e4e6efd1cd8a2667c792bd3e3
Kingslyno <kingslyno@gmail.com>
2024-11-17 21:09:26 +0100

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# For WSGI deployment
+def application(environ, start_response):
+    return app(environ, start_response)
